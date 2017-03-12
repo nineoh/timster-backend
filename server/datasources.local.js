98e04b08d8ac6e3efa7241319f76504370fb402a
derEisbaer <emil@effila.com>
2017-03-13 00:32:03 +0100

    added mongo db

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,7 +1,6 @@
-{
+module.exports = {
   db: {
-    defaultForType: 'mongodb';
-     connector: 'loopback-connector-mongodb';
-     url: process.env.MONGODB_URI;
-  };
+    connector: 'loopback-connector-mongodb',
+    url: process.env.MONGODB_URI
+  }
 }
