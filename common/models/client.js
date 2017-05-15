54c2dd8785a954cf12bf802449c3017ba3ce333c
derEisbaer <emil@effila.com>
2017-05-15 20:39:24 +0200

    added login functionality

--- common/models/client.js
+++ common/models/client.js
@@ -11,6 +11,28 @@ module.exports = function(Client) {
         returns: {arg: 'skills', type: 'array'}
     });
 
+    Client.login = (username, password, cb) => {
+
+        return Client.find({
+            where: {and: [{username: username } , {password: password}] }
+        }, (err, resp) => {
+                console.log('xClient.found resolve test err' , err);
+                console.log('xClient.found resolve test' , resp);
+                if(err) cb(null,{});
+
+                cb(null , resp);
+        });        
+    };
+
+    Client.remoteMethod('login', {
+        description: 'Login the client',
+        accepts: [
+            {arg: 'username', type: 'string'}, 
+            {arg: 'password', type: 'string'}
+            ],
+        returns: {type: 'object' , root: true}
+    });
+
 };
 
 
